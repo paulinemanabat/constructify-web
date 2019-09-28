9b9f42dfe62311647c2c76d51d68210484dd97e7
Pam Jasme <dhoreenpamjasme@gmail.com>
2019-09-29 02:21:59 +0800

    dashboard

--- src/app/modules/constructify/pages/dashboard/inventory/inventory.component.ts
+++ src/app/modules/constructify/pages/dashboard/inventory/inventory.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
+  title = 'Example of Angular 8 DataTable';
+  dtOptions: DataTables.Settings = {};
+  dtUsers =[
+    {"id": 101, "firstName": "Anil", "lastName": "Singh"},
+    {"id": 102, "firstName": "Reena", "lastName": "Singh"},
+    {"id": 103, "firstName": "Aradhay", "lastName": "Simgh"},
+    {"id": 104, "firstName": "Dilip", "lastName": "Singh"},
+    {"id": 105, "firstName": "Alok", "lastName": "Singh"},
+    {"id": 106, "firstName": "Sunil", "lastName": "Singh"},
+    {"id": 107, "firstName": "Sushil", "lastName": "Singh"},
+    {"id": 108, "firstName": "Sheo", "lastName": "Shan"},
+    {"id": 109, "firstName": "Niranjan", "lastName": "R"},
+    {"id": 110, "firstName": "Lopa", "lastName": "Mudra"},
+    {"id": 111, "firstName": "Paramanand","lastName": "Tripathi"}
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.dtOptions = {
+      data:this.dtUsers,
+      columns: [{title: 'User ID', data: 'id'},
+            {title: 'First Name', data: 'firstName'},
+            {title: 'Last Name', data: 'lastName' }]
+    };
   }
 
 }
